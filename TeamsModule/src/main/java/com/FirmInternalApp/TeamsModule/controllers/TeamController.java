0c45bf511ecb1ff56c50de5818889920973248bb
amirsabanovic <asabanovic3@etf.unsa.ba>
2017-03-28 23:22:06 +0200

    Cloud Config Server added
    Spring Centralized Cloud Config Server module added; TeamsModule
    controllers, services modified

--- TeamsModule/src/main/java/com/FirmInternalApp/TeamsModule/controllers/TeamController.java
+++ TeamsModule/src/main/java/com/FirmInternalApp/TeamsModule/controllers/TeamController.java
@@ -1,7 +1,5 @@
 package com.FirmInternalApp.TeamsModule.controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,34 +10,43 @@ import com.FirmInternalApp.TeamsModule.models.*;
 import com.FirmInternalApp.TeamsModule.services.TeamService;
 
 @RestController
+@RequestMapping(value = "/teams")
 public class TeamController {
 	
 	@Autowired
 	private TeamService teamService;
 
-    @RequestMapping(value = "/teams", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public Iterable<Team> getAllTeams() {
-    	List<Team> teams = new ArrayList<>();
-    	teamService.getAllTeams().forEach(teams::add);
-    	return teams;
+    	return teamService.getAllTeams();
     }
     
-    @RequestMapping(value = "/teams/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Team getTeamById(@PathVariable Long id) {
     	return teamService.getTeamById(id);
     }
     
-    @RequestMapping(value = "/teams", method = RequestMethod.POST)
+    @RequestMapping(value = "/byhandle/{handle}", method = RequestMethod.GET)
+    public Team getTeamByHandle(@PathVariable String handle) {
+    	return teamService.getTeamByHandle(handle);
+    }
+    
+    @RequestMapping(value = "/byname/{name}", method = RequestMethod.GET)
+    public Team getTeamByName(@PathVariable String name) {
+    	return teamService.getTeamByName(name);
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)
     public void addTeam(@RequestBody Team team) {
     	teamService.addTeam(team);
     }
     
-    @RequestMapping(value = "/teams/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public void updateTeam(@PathVariable Long id, @RequestBody Team team) {
-    	teamService.updateTeam(team);
+    	teamService.updateTeam(id, team);
     }
     
-    @RequestMapping(value = "/teams/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteTeam(@PathVariable Long id) {
     	teamService.deleteTeam(id);
     }
--- TeamsModule/src/main/java/com/FirmInternalApp/TeamsModule/services/TeamService.java
+++ TeamsModule/src/main/java/com/FirmInternalApp/TeamsModule/services/TeamService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.FirmInternalApp.TeamsModule.models.Team;
 import com.FirmInternalApp.TeamsModule.repositories.TeamRepository;
 
@@ -24,12 +25,24 @@ public class TeamService {
 		return teamRepository.findOne(id);
 	}
 	
+	public Team getTeamByHandle(String handle) {
+		return teamRepository.findByHandle(handle);
+	}
+	
+	public Team getTeamByName(String name) {
+		return teamRepository.findByName(name);
+	}
+	
 	public void addTeam(Team team) {
 		teamRepository.save(team);
 	}
 	
-	public void updateTeam(Team team) {
-		teamRepository.save(team);
+	public void updateTeam(Long id, Team team) {
+		Team teamToBeUpdated = teamRepository.findOne(id);
+		teamToBeUpdated.setHandle(team.getHandle());
+		teamToBeUpdated.setName(team.getName());
+		teamToBeUpdated.setInfo(team.getInfo());
+		teamRepository.save(teamToBeUpdated);
 	}
 	
 	public void deleteTeam(Long id) {
