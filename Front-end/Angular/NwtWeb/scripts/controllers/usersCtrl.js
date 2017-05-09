fa1e1a23df8bd7fa3243efee5275b8adcf01b6f5
Amra Hadziarapovic <ahadziarap1@etf.unsa.ba>
2017-05-09 19:26:50 +0200

    worked on users

--- Front-end/Angular/NwtWeb/scripts/controllers/usersCtrl.js
+++ Front-end/Angular/NwtWeb/scripts/controllers/usersCtrl.js
@@ -2,10 +2,18 @@
     var NwtWeb = angular.module('NwtWeb');
 
     NwtWeb.controller('usersController',  ['$rootScope', '$scope', '$location', 'dataService', function ($rootScope, $scope, $location, dataService) {
+        $scope.editUser = {
+            name: "",
+            username: "",
+            profileImg: "",
+            id:0
+        }
         $scope.users =
         [
         {
             name: "Amra",
+            username: "amrausername",
+            profileImg: "https://www.w3schools.com/bootstrap/cinqueterre.jpg",
             id: 5,
         },
         {
@@ -14,6 +22,14 @@
         }
         ]
         console.log($scope.users);
+        $scope.transferData = function (user) {
+            $scope.editUser.id = user.id;
+            $scope.editUser.name = user.name;
+            $scope.editUser.profileImg = user.profileImg;
+            $scope.editUser.username = user.username;
+            console.log($scope.editUser);
+
+        }
         //$scope.loadUsers = function () {
         //    dataService.list("users", function (data) {
         //        if (data) {
