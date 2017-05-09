0956a2f51bca873517c277d47dba21e18699434f
Amra Hadziarapovic <ahadziarap1@etf.unsa.ba>
2017-05-09 18:23:00 +0200

    worked on users

--- Front-end/Angular/NwtWeb/scripts/controllers/usersCtrl.js
+++ Front-end/Angular/NwtWeb/scripts/controllers/usersCtrl.js
@@ -2,6 +2,24 @@
     var NwtWeb = angular.module('NwtWeb');
 
     NwtWeb.controller('usersController',  ['$rootScope', '$scope', '$location', 'dataService', function ($rootScope, $scope, $location, dataService) {
-        
+        $scope.users =
+        [
+        {
+            name: "Amra",
+            id: 5,
+        },
+        {
+            name: "Irma",
+            id:3
+        }
+        ]
+        console.log($scope.users);
+        //$scope.loadUsers = function () {
+        //    dataService.list("users", function (data) {
+        //        if (data) {
+        //            $scope.users = data;
+        //        }
+        //    })
+        //};
     }]);
 }());
