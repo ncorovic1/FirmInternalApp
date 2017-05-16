ad76284f583560fc8be597f1b33300c6447bec0d
Irma Karasoftic <irmaka@maestralsolutions.com>
2017-05-16 17:33:47 +0200

    worked on vacations

---
+++ Front-end/Angular/NwtWeb/scripts/controllers/datePickerCtrl.js
@@ -0,0 +1,97 @@
+﻿(function () {
+    'use strict';
+
+    var NwtWeb= angular.module("NwtWeb");
+
+    NwtWeb.controller("datepickerController", ['$scope', function ($scope) {
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+        $scope.altInputFormats = ['M!/d!/yyyy'];
+
+        $scope.dateOptions = {
+            formatYear: 'yy',
+            startingDay: 1
+        };
+
+        $scope.maxDate = null;
+        $scope.minDate = new Date();
+        $scope.maxDateEmp = new Date();
+        $scope.minDateEmp = null;
+        $scope.maxDateBirth = new Date();
+        $scope.minDateBirth = null;
+        $scope.maxDateRequest = null;
+        $scope.minDateRequest = new Date('01/01/2010'),
+            $scope.startDate = new Date();
+        $scope.endDate = new Date();
+        $scope.whenDate = new Date();
+
+        $scope.disabled = function (date, mode) {
+            return (mode === 'day' && (date.getDay() === 0 || date.getDay() === 6));
+        };
+
+        $scope.openUntil = function () {
+            $scope.popupUntil.opened = true;
+        };
+
+        $scope.openStart = function () {
+            $scope.popupStart.opened = true;
+        };
+
+        $scope.openSkip = function () {
+            $scope.popupSkip.opened = true;
+        };
+
+        $scope.openFrom = function () {
+            $scope.popupFrom.opened = true;
+        };
+
+        $scope.openTo = function () {
+            $scope.popupTo.opened = true;
+        };
+
+        $scope.openEnd = function () {
+            $scope.popupEnd.opened = true;
+        };
+
+        $scope.openEmp = function () {
+            $scope.popupEmp.opened = true;
+        };
+
+        $scope.openBirth = function () {
+            $scope.popupBirth.opened = true;
+        };
+
+        $scope.popupUntil = {
+            opened: false
+        };
+
+        $scope.popupStart = {
+            opened: false
+        };
+
+        $scope.popupSkip = {
+            opened: false
+        };
+
+        $scope.popupFrom = {
+            opened: false
+        };
+
+        $scope.popupTo = {
+            opened: false
+        };
+
+        $scope.popupEnd = {
+            opened: false
+        };
+
+        $scope.popupEmp = {
+            opened: false
+        };
+
+        $scope.popupBirth = {
+            opened: false
+        };
+    }]);
+}());
--- Front-end/Angular/NwtWeb/scripts/controllers/vacationsCtrl.js
+++ Front-end/Angular/NwtWeb/scripts/controllers/vacationsCtrl.js
@@ -1,7 +1,77 @@
-﻿(function () {
+﻿(function() {
     var NwtWeb = angular.module('NwtWeb');
-    NwtWeb.controller('vacationsController',  ['$rootScope', '$scope', '$location', 'dataService', function ($rootScope, $scope, $location, dataService)  {
-       
+    NwtWeb.controller('vacationsController',
+        [
+            '$rootScope', '$scope', '$location', 'dataService', function($rootScope, $scope, $location, dataService) {
 
-    }]);
-}());
+                $scope.requestItem = {
+                    id: 0,
+                    employeeId: $rootScope.currentUser.id,
+                    eventType: 0,
+                    startDate: null,
+                    endDate: null,
+                    note: "",
+                    approved: false,
+                    isCanceled: false
+                };
+
+
+                function fetchMyEvents() {
+                    $scope.waitEvents = true;
+                    dataService.read("events",
+                        $rootScope.currentUser.id,
+                        function(data) {
+                            if (data) {
+                                $scope.myEvents = data;
+                                $scope.waitEvents = false;
+                            } else {
+                                notificationsConfig.error(constants.notificationMessages.ERROR_ABSENCE_DAYS_NOT_LOADED);
+                            }
+                        });
+                };
+
+                function fetchEventTypes() {
+                    vacationsService.list("eventtypes",
+                        function(data) {
+                            $scope.eventTypes = data;
+                            $scope.eventDescriptions = data;
+                        });
+                };
+
+                $scope.createRequest = function() {
+                    dataService.create("events",
+                        $scope.requestItem,
+                        function(data) {
+                            if (data) {
+                                $scope.processingRequest = false;
+                                if ($scope.requestItem.eventType === 12) {
+                                    notificationsConfig.success("Remote work successfully recorded");
+                                } else {
+                                    notificationsConfig.success(constants.notificationMessages
+                                        .SUCCESS_ABSENCE_DAY_REQUEST);
+                                }
+                                fetchMyEvents();
+                                $scope.clearRequest();
+                            } else {
+                                $scope.processingRequest = false;
+                                notificationsConfig.error($rootScope.message);
+                            }
+                        });
+                }
+
+                $scope.clearRequest = function() {
+                    $scope.requestItem = {
+                        id: 0,
+                        employeeId: $rootScope.currentUser.id,
+                        eventType: 0,
+                        startDate: null,
+                        endDate: null,
+                        note: "",
+                        approved: false,
+                        isCanceled: false
+                    }
+                    $scope.showDescription = false;
+                };
+            }
+        ]);
+});
