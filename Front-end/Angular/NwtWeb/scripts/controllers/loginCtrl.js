b654cc71f8a3f2766a0557e13dee85554de7a6b8
Irma Karasoftić <irma.karasoftic@gmail.com>
2017-05-31 01:14:32 +0200

    login

--- Front-end/Angular/NwtWeb/scripts/controllers/loginCtrl.js
+++ Front-end/Angular/NwtWeb/scripts/controllers/loginCtrl.js
@@ -10,63 +10,76 @@
             isAdmin: false
         }
         $scope.loginCheck = function (un, pw) {
-            var req = {
-                method: 'POST',
-                url: "http://localhost:8085/login",
-                headers: {
-                    "Content-type": "application/json; charset=utf-8"
-                },
-                data: 
-                {
-                    'username': 'irma',
-                    'password': 'password'
+            var creds = { 'username': 'irma', 'password': 'password' }
+            $http.post('http://localhost:8085/login', creds, 
+                            {
+                                headers: {
+                                    'Content-Type': 'text/plain',
+                                },
+                                transformResponse: undefined
+                            })
+                            .then(response => {
+                                console.log(response);
+                                console.log(response.status);
+                                console.log(response.headers);
+                                console.log(response.headers.get('Authorization'));
+                                $http.defaults.headers.common.Authorization = 'Bearer ' + data.data.access_token;
+                                $cookies.put("access_token", data.data.access_token);
+                            }, error => {
+                         console.log(error)});
+            //$http({
+            //    method:'POST',
+            //    url: 'http://localhost:8085/login',
+            //    contentType: "application/json",
+            //    data: angular.toJson({ username: 'irma', password: 'password' }, true),
+            //    transformResponse:undefined,
+            //}).then(function(result) {
+            //    console.log(result.data);
+            //    $scope.message = result;
+            //}, function(error) {
+            //    console.log(error);
+            //});
+		
+        //    $window.location.href = '/users';
+        };
+       
+    function handleSuccess() {
+        return { success: true };
+    }
+
+    function handleSuccessUsera(response) {
+        return response.data;
+    }
+
+    function handleError(error) {
+        return function () {
+            return { success: false, message: error };
+        };
+    }
+    $scope.login = function () {
+        var response = $scope.loginCheck();
+        $rootScope.token = response;
+        $window.localStorage.setItem("authdata", $scope.login.username + response);
+        $http.defaults.headers.common.Authorization = 'Bearer ' + response;
+
+        $log.log(response);
+        $rootScope.isLoggedIn = true;
+        $location.path('/users');
+    }
+
+    $scope.sendResetPasswordEmail = function (email) {
+        loginService.create("users", email,
+            function (data) {
+                if (data) {
+                    alert("Reset password email sent!");
+                } else {
+                    alert("There was an error!");
                 }
-            }
-            $http(req).then(function (data) {
-                $http.defaults.headers.common.Authorization =
-                  'Bearer ' + data.data.access_token;
-                $cookies.put("access_token", data.data.access_token);
-                window.location.href = "index";
             });
-
-        }
-        function handleSuccess() {
-            return { success: true };
-        }
-
-        function handleSuccessUsera(response) {
-            return response.data;
-        }
-
-        function handleError(error) {
-            return function () {
-                return { success: false, message: error };
-            };
-        }
-        $scope.login = function () {
-            var response = $scope.loginCheck();
-            $rootScope.token = response;
-            $window.localStorage.setItem("authdata", $scope.login.username + response);
-            $http.defaults.headers.common.Authorization = 'Bearer ' + response;
-
-            $log.log(response);
-            $rootScope.isLoggedIn = true;
-            $location.path('/users');
-        }
-
-        $scope.sendResetPasswordEmail = function (email) {
-            loginService.create("users", email,
-                function (data) {
-                    if (data) {
-                        alert("Reset password email sent!");
-                    } else {
-                        alert("There was an error!");
-                    }
-                });
-        }
-        $scope.cancel = function () {
-            $scope.emailInput = "";
-        }
-    }]);
+    }
+    $scope.cancel = function () {
+        $scope.emailInput = "";
+    }
+}]);
 
 }());
