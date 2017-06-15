d0baf76b118f85ad8fed5f90b682d3b417877a0a
Irma Karasoftić <irma.karasoftic@gmail.com>
2017-06-16 01:12:12 +0200

    teams

--- Front-end/Angular/NwtWeb/scripts/controllers/teamsCtrl.js
+++ Front-end/Angular/NwtWeb/scripts/controllers/teamsCtrl.js
@@ -2,8 +2,10 @@
     var NwtWeb = angular.module('NwtWeb');
 
     NwtWeb.controller('teamsController', ['$rootScope', '$scope', '$location', 'teamService', '$http', 'dataService', function ($rootScope, $scope, $location, teamService, $http, dataService) {
-        
-        $scope.loadUsers = function () {
+
+        $scope.tempTeamToAddUsers = 0;
+
+        $scope.loadTeams = function () {
             $scope.waitUsers = true;
             teamService.list("teams",
                 function (data) {
@@ -15,7 +17,7 @@
                     }
                 });
         };
-        $scope.loadUsers();
+        $scope.loadTeams();
         $scope.getTeamMembers = function (id) {
             $scope.waitUsers = true;
             dataService.read("users/byteam", id, function (data) {
@@ -27,7 +29,29 @@
                 }
             });
         };
-
+        $scope.getAllUsers = function (team) {
+            dataService.list("users",
+                function (data) {
+                    if (data) {
+                        $scope.tempUsers = data;
+                        $scope.tempTeamToAddUsers = team;
+                    }
+                    else {
+                        alert("Error!");
+                    }
+                });
+        };
+        $scope.getAllUsers();
+        $scope.addUserToTeam = function (user) {
+            user.team = $scope.tempTeamToAddUsers;
+            dataService.update("users", user.id, user, function (data) {
+                if (data) {
+                    alert("User updated");
+                }
+                else
+                    alert("Error");
+            });
+        };
     }]);
 
 }());
